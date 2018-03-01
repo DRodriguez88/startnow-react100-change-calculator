5aef1f2711de0b6898ca6cbf79ab36f49849c76d
Dj <rodriguezdanilo88@gmail.com>
2018-02-28 19:10:00 -0800

    in progress

--- src/app.jsx
+++ src/app.jsx
@@ -6,6 +6,14 @@ class App extends Component {
   }
 
   render() {
+    return (
+      <div id="bigMomma" className="container">
+        <div className="pt-4">
+          <h1 className="text-white">Change Calculator</h1>
+          <hr className="border-white"></hr>
+        </div>
+      </div>
+    )
   }
 }
 

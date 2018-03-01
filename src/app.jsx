26a2b8bf383703908c369f4365c887a737fbdaaa
Dj <rodriguezdanilo88@gmail.com>
2018-02-28 20:30:45 -0800

    first card finished

--- src/app.jsx
+++ src/app.jsx
@@ -7,11 +7,31 @@ class App extends Component {
 
   render() {
     return (
-      <div id="bigMomma" className="container">
-        <div className="pt-4">
+      <div id="bigMomma" className="container-fluid">
+        <div className="p-4">
           <h1 className="text-white">Change Calculator</h1>
           <hr className="border-white"></hr>
         </div>
+
+        <div className="p-2">
+          <div className="col-sm-4">
+            <div className="card">
+              <div className="card-header">
+                Enter Information
+              </div>
+              <div className="card-body">
+                <p className="font-weight-bold">How much is due?</p>
+                <input id="due" className="form-control"/>
+                <p className="font-weight-bold">How much was received?</p>
+                <input id="received" className="form-control"/>
+              </div>
+              <div className="card-footer">
+                <button className="btn btn-primary btn-lg btn-block">Calculate</button>
+              </div>
+            </div>
+          </div>   
+        </div>
+
       </div>
     )
   }
